767d32c5998ff99e1d1bb0bb8d4f5d9c6144b605
Kimberley Bordier <97187997+KimberleyBdr@users.noreply.github.com>
2024-09-28 17:28:59 +0200

    Update survey.js

--- controllers/survey.js
+++ controllers/survey.js
@@ -1,14 +1,7 @@
-const surveyData = {
-    id: 1,
-    title: "Survey Title",
-    questions: [
-        { question: "Question 1?", options: ["Option 1", "Option 2"] },
-        // Ajoute d'autres questions ici
-    ]
-};
+const survey = require('../models/survey');
 
 const getSurvey = () => {
-    return surveyData; // ou une logique pour récupérer des données réelles
+    return survey; // Retourne l'ensemble des questions du survey
 };
 
 module.exports = getSurvey;
